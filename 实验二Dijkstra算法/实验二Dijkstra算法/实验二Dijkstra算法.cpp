bcd29fc77c6f5cb3746515f2c44451bbc1f10f7f
LonelyTaker <1104340863@qq.com>
2020-03-07 20:58:14 +0800

    实验二

---
+++ 实验二Dijkstra算法/实验二Dijkstra算法/实验二Dijkstra算法.cpp
@@ -0,0 +1,59 @@
+﻿// 实验二Dijkstra算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+8
+2000 1 2000 2000 2000 2000 2000 2000
+2000 2000 2000 2 2000 2000 2000 2000
+2 2000 2000 2000 2000 2000 2000 2000
+2000 2000 1 2000 2000 8 2000 2000
+2000 2000 2000 2 2000 2000 2 2000
+2000 2000 2000 2000 2 2000 2000 2000
+2000 2000 2000 2000 2000 3 2000 3
+2000 2000 2000 2000 2000 2 2000 2000
+*/
+
+#include "pch.h"
+#include <iostream>
+#define MAXG 100
+
+void Dijkstra(int a, int b, int n, int graph[][MAXG]) {
+	int dis[MAXG], visit[MAXG], min_g, min_weight;
+	for (int i = 0; i < n; i++)
+	{
+		dis[i] = graph[a][i];
+		visit[i] = 0;
+	}
+	visit[a] = 1;
+	for (int j = 0; j < n-2; j++) {
+		min_weight = 2000;
+		min_g = -1;
+		for (int i = 0; i < n; i++){
+			if (dis[i] < min_weight&&visit[i] == 0) {
+			min_weight = dis[i];
+			min_g = i;
+			}
+		}
+		visit[min_g] = 1;
+		for (int i = 0; i < n; i++){
+			if (graph[min_g][i] + min_weight < dis[i] && visit[i] == 0) {
+				dis[i] = graph[min_g][i] + min_weight;
+			}
+		}
+	}
+	printf("%d\n", dis[b]);
+}
+
+int main()
+{
+	int graph[MAXG][MAXG];
+	int n;
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; ++j) {
+			scanf_s("%d", &graph[i][j]);
+		}
+	}
+	Dijkstra(0, 7, n, graph);
+}
+
---
+++ 实验二Floyd算法/实验二Floyd算法/实验二Floyd算法.cpp
@@ -0,0 +1,64 @@
+﻿// 实验二Floyd算法.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+4
+0 2 6 4
+2000 0 3 2000
+7 2000 0 1
+5 2000 12 0
+*/
+
+#include "pch.h"
+#include <iostream>
+#define MAXG 100
+
+void fun(int n, int graph[][MAXG]) {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf_s("%d ", graph[i][j]);
+		}
+		printf_s("\n");
+	}
+	printf_s("\n");
+}
+
+void Floyd(int n,int graph[][MAXG]) {
+	int A[MAXG][MAXG];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; ++j) {
+			A[i][j] = graph[i][j];
+		}
+	}
+	for (int v = 0; v < n; v++)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n;++j) {
+				if (A[i][j] > A[i][v] + A[v][j]) {
+					A[i][j] = A[i][v] + A[v][j];
+				}
+			}
+		}
+	}
+	fun(n, A);
+}
+
+int main()
+{
+	int n;
+	int graph[MAXG][MAXG];
+	scanf_s("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n;++j) {
+			scanf_s("%d", &graph[i][j]);//输入图，如果点i到点j没有直接路径，输入无穷大
+		}
+	}
+	Floyd(n,graph);
+	return 0;
+}
+
+
